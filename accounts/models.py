17ca61b8b1d7e8a8ee05e2fc8a7d7e306568fdb2
Llanz Kenneth Luy <llanzkennethluy@gmail.com>
2022-08-07 21:55:12 +0800

    Create an informative author and profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 from django.db import models
 from PIL import Image
 # Create your models here.
@@ -8,6 +9,8 @@ class Profile(models.Model):
     province = models.CharField(max_length=30, blank=True, null=True)
     phone = models.CharField(max_length=11, blank=True, null=True)
     profile_picture = models.ImageField(default='default.jpg', upload_to='profile_pics', blank=True, null=True)
+    job_description = models.TextField(max_length=200, blank=True, null=True)
+    about_info = RichTextField(blank=True, null=True)
     user_slug = models.SlugField(unique=True, default=None)
 
     def __str__(self):
