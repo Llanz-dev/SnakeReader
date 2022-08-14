47ea1b08ed8ed652a9c5ccc855ecbef94604e801
Llanz Kenneth Luy <llanzkennethluy@gmail.com>
2022-08-14 16:18:40 +0800

    home page design complete

--- snakereader/settings.py
+++ snakereader/settings.py
@@ -83,7 +83,6 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
-AUTH_USER_MODEL = 'accounts.myUser'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
