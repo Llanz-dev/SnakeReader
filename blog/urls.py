69df3d6531b9337841ac7c75422672c1026e671c
Llanz Kenneth Luy <llanzkennethluy@gmail.com>
2022-09-20 20:34:06 +0800

    delete article and profile

--- blog/urls.py
+++ blog/urls.py
@@ -7,13 +7,15 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('blog-detail/<slug:slug>/', views.blog_detail, name='blog_detail'),
     path('create-article/', views.create_article, name='create_article'),
+    path('delete-article/<int:pk>/<slug:slug>/', views.delete_article, name='delete_article'),
     path('about/', views.about, name='about'),
     path('add-comment/<slug:slug>/', views.add_comment, name='add_comment'),
     path('delete-comment/<int:pk>/<slug:slug>/', views.delete_comment, name='delete_comment'),
     path('edit-comment/<int:pk>/<slug:slug>/', views.edit_comment, name='edit_comment'),
     path('contact/', views.contact, name='contact'),
     path('author/<slug:user_slug>/', views.author, name='author'),
-    path('category/<slug:slug>/', views.specific_category, name='specific_category'),
+    path('category/<slug:slug>/', views.specific_category, name='specific_category'),    
+    path('title-duplicate-error/<str:title_entered>/', views.title_duplicate_error, name='title_duplicate_error'),    
 ]
 
 urlpatterns += staticfiles_urlpatterns()
